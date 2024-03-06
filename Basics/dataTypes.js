008b9592e9e63657c9fb2f9f92cca2f2fceb0911
Raja Gupta <rajaggupta5@gmail.com>
2024-03-06 16:09:36 +0530

    DataTypes in js

---
+++ Basics/dataTypes.js
@@ -0,0 +1,31 @@
+"use strict" //Treat all JS Code as a newer version
+
+// alert( 3 + 3)  We are using node.js not browser
+
+
+console.log( 3
+        + 
+        3)  //Code readability should be high
+
+console.log("Raja")
+
+
+let name = "Raja"
+let age = 21
+let isLoggedIn = false
+let state;
+
+// DataTypes
+//number =>2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => stamdalone value
+//undefined
+//symbol => unique
+
+
+// objects
+
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
