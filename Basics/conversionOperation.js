53dcb83b871728f90069a11c7b1b6d8c8227a79e
Raja Gupta <rajaggupta5@gmail.com>
2024-03-07 20:47:19 +0530

    Updated Conversion Operations

--- Basics/conversionOperation.js
+++ Basics/conversionOperation.js
@@ -25,5 +25,45 @@ let someNumber = 21
 
 let stringNumber = String(someNumber)
 
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+// ************************ Operations *******************************
+let value = 3
+let negValue = -3
+
+console.log(-value)
+
+
+// console.log(2+2)
+// console.log(2-3)
+// console.log(3/2)
+// console.log(2*3)
+// console.log(2**3) //Power
+// console.log(3%2)
+
+// Concatenation
+// let str1 = "Hello"
+// let str2 = " Raja"
+// let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// console.log(2 + 2 + "1")
+
+// console.log((3+4) * 5 % 3)
+
+
+console.log(+true)
+console.log(+"")
+
+let num1, num2, num3
+
+num1 = num2 = num3  = 2 + 2
+
+
+let gameCounter = 100
+// console.log(gameCounter++) //Postfix
+console.log(++gameCounter) //Prefix
