7cacd6ddc63644a81c63354b009fb0717e65b336
Raja Gupta <rajaggupta5@gmail.com>
2024-03-24 12:52:07 +0530

    Switch Case in js

---
+++ control_flow_04/switch.js
@@ -0,0 +1,18 @@
+let key = 3;
+switch (key) {
+    case 1:
+        console.log('January');
+        break;
+    case 2:
+        console.log("February");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+
+    default:
+        console.log("Default case match!");
+}
